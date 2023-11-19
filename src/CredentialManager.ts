c5d2e016b4d0663a8d734374683b813c1b136fba
techchrism <chris@techchrism.me>
2023-11-19 07:40:53 -0800

    add reauth attempts

--- src/CredentialManager.ts
+++ src/CredentialManager.ts
@@ -28,12 +28,7 @@ export class CredentialManager {
         return this._entitlement!
     }
 
-    private async _renewCredentials(): Promise<void> {
-        this._logger.info('Refreshing credentials...')
-        if(this._ssid === null) {
-            this._ssid = await fs.readFile('./ssid.txt', 'utf-8')
-        }
-
+    private async _attemptReauth() {
         const response = await fetch('https://auth.riotgames.com/authorize?redirect_uri=https%3A%2F%2Fplayvalorant.com%2Fopt_in&client_id=play-valorant-web-prod&response_type=token%20id_token&nonce=1', {
             method: 'GET',
             redirect: 'manual',
@@ -46,13 +41,47 @@ export class CredentialManager {
         this._logger.verbose(`Response status: ${response.status}`)
         this._logger.verbose(`Response headers: ${JSON.stringify(response.headers.raw())}`)
 
+        const location = response.headers.get('location')
+        if(location === null) throw new Error('No location header in response!')
+        if(!location.startsWith('https://playvalorant.com/opt_in')) {
+            this._logger.error(`Reauth failed, location: ${location}`)
+            throw new Error('Invalid reauth location!')
+        }
+
         const searchParams = new URLSearchParams((new URL(response.headers.get('location')!)).hash.slice(1))
 
-        this._token = searchParams.get('access_token')
-        this._entitlement = searchParams.get('id_token')
+        //TODO further validation on these values
+        const token = searchParams.get('access_token')
+        const entitlement = searchParams.get('id_token')
+        const ssid = response.headers.get('set-cookie')!.match(ssidRegex)![1]
+
+        this._token = token
+        this._entitlement = entitlement
         this._expiration = (Number(searchParams.get('expires_in')) * 1000) + Date.now() - expirationDiff
         this._logger.info(`Credentials refreshed, expires at ${new Date(this._expiration)}`)
-        this._ssid = response.headers.get('set-cookie')!.match(ssidRegex)![1]
+        this._ssid = ssid
+    }
+
+    private async _renewCredentials(): Promise<void> {
+        this._logger.info('Refreshing credentials...')
+        if(this._ssid === null) {
+            this._ssid = await fs.readFile('./ssid.txt', 'utf-8')
+        }
+
+        let delay = 0
+        let attemptCount = 0
+        while(attemptCount < 10) {
+            attemptCount++
+            try {
+                await this._attemptReauth()
+                break
+            } catch (e) {
+                this._logger.error(`Failed to reauth: ${e}`)
+                this._logger.info(`Retrying in ${delay}ms...`)
+                await new Promise(resolve => setTimeout(resolve, delay))
+                delay = Math.min(delay + 1000, 5000)
+            }
+        }
 
         // Re-write ssid.txt
         await fs.writeFile('./ssid.txt', this._ssid)
